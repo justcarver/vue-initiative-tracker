14a782c3c1be09748269c71b9bc19ca84b45f364
Justin Carver <carver@justcarver.com>
2018-12-30 16:16:17 -0500

    Added Initiative Tracker Component

---
+++ src/components/InitiativeTracker.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="initiative-tracker">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InitiativeTracker',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
